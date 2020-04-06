d04ff34f56db8f85db478bfb16cc50c419e64172
kannana-mitel <kannana@akdriods-mac-2.lan>
2020-04-06 14:22:22 -0400

    Initial Setup

---
+++ init.js
@@ -0,0 +1,12 @@
+// script to init.
+// Need to do the following to avoid devs to have to run a bunch of commands
+
+//Add core repo
+// git subtree add   -P 'whatIsThis' https://github.com/kannana-mitel/primary.git master --squash
+
+
+//Pull core repo
+// git subtree pull   -P 'whatIsThis' https://github.com/kannana-mitel/primary.git master --squash
+
+//Merge core repo
+// git subtree merge   -P 'whatIsThis' https://github.com/kannana-mitel/primary.git master --squash
